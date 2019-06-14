2e5aedf5f27ebe3421f75ae5c7eacb3f5cd70278
Hollis Ma <hollism@princeton.edu>
2019-06-14 16:48:08 -0400

    added user model and user registration (user post)

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    require: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// mongoose.model takes in the name of the model and the schema
+module.exports = User = mongoose.model('user', UserSchema);
