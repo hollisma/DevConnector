d16412c7d3bb6fcb3f0c5296a764d75de97c67ea
Hollis Ma <hollism@princeton.edu>
2019-06-16 15:28:38 -0400

    added alert reducer, action, types, and component

---
+++ client/src/reducers/alert.js
@@ -0,0 +1,18 @@
+import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
+
+// Array of alerts. Each alert has an id, msg, and type(success/failure)
+const initialState = [];
+
+// Action has type and payload(data)
+export default function (state = initialState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case SET_ALERT:
+      return [...state, payload];
+    case REMOVE_ALERT:
+      return state.filter(alert => alert.id !== payload)
+    default:
+      return state;
+  }
+}
