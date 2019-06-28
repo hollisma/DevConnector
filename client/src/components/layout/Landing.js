c7b9069a27a574c74ee0ecdf2d23b6be87e879b6
Hollis Ma <hollism@princeton.edu>
2019-06-27 21:59:38 -0400

    updated to LearnMeCode

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -13,10 +13,10 @@ const Landing = ({ isAuthenticated }) => {
       <section className="landing">
         <div className="dark-overlay">
           <div className="landing-inner">
-            <h1 className="x-large">Developer Connector</h1>
+            <h1 className="x-large">Learn Me Code</h1>
             <p className="lead">
-              Create a developer profile/portfolio, share posts and get help
-              from other developers
+              Join other new coders to discover the wonderful world of coding
+              together
             </p>
             <div className="buttons">
               <Link to="/register" className="btn btn-primary">
